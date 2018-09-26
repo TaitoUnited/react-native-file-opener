2f381d4a00a9b1635f0d57b611c85813dd95ba06
Temzasse <teemu.taskula@taitounited.fi>
2018-09-26 22:53:27 +0300

    fix: ios bridge import error

--- ios/RNFileOpener/RNFileOpener.h
+++ ios/RNFileOpener/RNFileOpener.h
@@ -1,4 +1,9 @@
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else // back compatibility for RN version < 0.40
 #import "RCTBridgeModule.h"
+#endif
+
 #import "RCTBridge.h"
 
 @import UIKit;
